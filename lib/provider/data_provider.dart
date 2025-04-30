970997a056f2418d1306b9a0e2e23da929b050cf
pizi-0 <hafizirahim92@gmail.com>
2025-04-30 11:09:22 +0800

    add pinnedAppProvider

--- lib/provider/data_provider.dart
+++ lib/provider/data_provider.dart
@@ -1,7 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../models/app_config_pair.dart';
+
 final devicesProvider = StateProvider((ref) => []);
 
 final configsProvider = StateProvider((ref) => []);
 
 final instancesProvider = StateProvider((ref) => []);
+
+final pinnedAppProvider = StateProvider<List<AppConfigPair>>((ref) => []);
