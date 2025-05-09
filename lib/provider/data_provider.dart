47f6d0801dfb6615ee93bf7191efc2e22dc8c6ad
pizi-0 <hafizirahim92@gmail.com>
2025-05-09 22:09:16 +0800

    add type

--- lib/provider/data_provider.dart
+++ lib/provider/data_provider.dart
@@ -1,11 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:scrcpygui_companion/models/companion_server/data/config_payload.dart';
+import 'package:scrcpygui_companion/models/companion_server/data/device_payload.dart';
+import 'package:scrcpygui_companion/models/companion_server/data/instance_payload.dart';
+import 'package:scrcpygui_companion/models/companion_server/data/pairs_payload.dart';
 
-import '../models/app_config_pair.dart';
+final devicesProvider = StateProvider<List<DevicePayload>>((ref) => []);
 
-final devicesProvider = StateProvider((ref) => []);
+final configsProvider = StateProvider<List<ConfigPayload>>((ref) => []);
 
-final configsProvider = StateProvider((ref) => []);
+final instancesProvider = StateProvider<List<InstancePayload>>((ref) => []);
 
-final instancesProvider = StateProvider((ref) => []);
-
-final pinnedAppProvider = StateProvider<List<AppConfigPair>>((ref) => []);
+final pinnedAppProvider = StateProvider<List<PairsPayload>>((ref) => []);
