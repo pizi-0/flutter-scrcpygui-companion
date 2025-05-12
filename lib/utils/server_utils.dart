12a1f984df45366b2e50c6720724142bf35ef7ee
pizi-0 <hafizirahim92@gmail.com>
2025-05-12 20:50:38 +0800

    new auth

--- lib/utils/server_utils.dart
+++ lib/utils/server_utils.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:encrypt_decrypt_plus/encrypt_decrypt/xor.dart';
 import 'package:scrcpygui_companion/models/companion_server/client_payload.dart';
+import 'package:scrcpygui_companion/models/companion_server/data/auth_payload.dart';
 import 'package:scrcpygui_companion/models/server_model.dart';
 
 class ServerUtils {
@@ -17,8 +19,14 @@ class ServerUtils {
 
   connect(ServerModel server) async {
     socket = await Socket.connect(server.ip, server.port);
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    final androidInfo = await deviceInfo.androidInfo;
+    final deviceName = androidInfo.name;
+    final deviceModel = androidInfo.model;
 
-    socket.write(XOR().xorEncode(server.secret));
+    socket.write(
+      '${AuthPayload(deviceName: deviceName, deviceModel: deviceModel, apikey: XOR().xorEncode(server.secret)).toJson()}\n',
+    );
   }
 
   disconnect() async {
