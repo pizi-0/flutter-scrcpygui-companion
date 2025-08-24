c3bf6d70f94c1bb12d8f8d22aed370a504c169e7
pizi-0 <hafizirahim92@gmail.com>
2025-08-24 16:16:18 +0800

    ghpage

--- android/app/src/main/kotlin/com/example/flutter_scrcpygui_companion/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.pizi.scrcpygui.companion
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/utils/pref_keys.dart
+++
@@ -1,5 +0,0 @@
-// ignore_for_file: constant_identifier_names
-
-const PKEY_SERVER_LIST = 'server-list';
-
-const PKEY_LAST_IP_INPUT = 'last-ip-input';
